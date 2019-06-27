95b383422865234c33c7e2166fd65c92ab20eaa4
taccisum <liaojinfeng@deepexi.com>
2019-06-28 01:06:50 +0800

    optimize code format

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,34 +4,19 @@ const path = require('path');
 
 const obj = {
   groupId: {
-    prompting: {
-      type: 'input',
-      message: '请输入你的group id',
-      default: 'com.deepexi'
-    },
+    prompting: { type: 'input', message: '请输入你的group id', default: 'com.deepexi' },
     option: { desc: 'group id', type: String, default: 'com.deepexi' }
   },
   artifactId: {
-    prompting: {
-      type: 'input',
-      message: '请输入你的artifact id',
-      default: 'deepexi-spring-cloud'
-    },
+    prompting: { type: 'input', message: '请输入你的artifact id', default: 'deepexi-spring-cloud' },
     option: { desc: 'artifact id', type: String, default: 'deepexi-spring-cloud' }
   },
   basePackage: {
-    prompting: {
-      type: 'input',
-      message: '请输入你的基础包路径（为空则使用group id）'
-    },
+    prompting: { type: 'input', message: '请输入你的基础包路径（为空则使用group id）' },
     option: { desc: '基础包路径（为空则使用group id）', type: String, default: '' }
   },
   mavenUrl: {
-    prompting: {
-      type: 'input',
-      message: '请输入你的maven仓库地址',
-      default: 'http://nexus.deepexi.top/repository/maven-public/'
-    },
+    prompting: { type: 'input', message: '请输入你的maven仓库地址', default: 'http://nexus.deepexi.top/repository/maven-public/' },
     option: { desc: 'maven仓库url', type: String, default: 'http://nexus.deepexi.top/repository/maven-public/' }
   }
 }
