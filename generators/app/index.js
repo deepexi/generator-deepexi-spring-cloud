43ba23bee7304b033df8462881b20159a689af52
taccisum <liaojinfeng@deepexi.com>
2019-06-27 23:10:33 +0800

    support specify base package

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,10 +14,25 @@ const obj = {
   artifactId: {
     prompting: {
       type: 'input',
-      message: '请输入你的app id',
+      message: '请输入你的artifact id',
       default: 'deepexi-spring-cloud'
     },
-    option: { desc: '项目名称', type: String, default: 'deepexi-spring-cloud' }
+    option: { desc: 'artifact id', type: String, default: 'deepexi-spring-cloud' }
+  },
+  basePackage: {
+    prompting: {
+      type: 'input',
+      message: '请输入你的基础包路径（为空则使用group id）'
+    },
+    option: { desc: 'artifact id', type: String, default: '' }
+  },
+  mavenUrl: {
+    prompting: {
+      type: 'input',
+      message: '请输入你的maven仓库地址',
+      default: 'http://nexus.deepexi.top/repository/maven-public/'
+    },
+    option: { desc: 'maven仓库url', type: String, default: 'http://nexus.deepexi.top/repository/maven-public/' }
   }
 }
 
@@ -25,8 +40,9 @@ module.exports = require('yo-power-generator').getGenerator(obj, {
   handlerDir: path.join(__dirname, 'handler'),
   templateDir: path.join(__dirname, 'templates'),
   beforeWrite (props) {
-    if (!props.basePath) {
-      props.basePath = props.groupId.replace('.', '/');
+    if (!props.basePackage) {
+      props.basePackage = props.groupId;
     }
+    props.basePath = props.basePackage.replace(/\./g, '/');
   }
 });
