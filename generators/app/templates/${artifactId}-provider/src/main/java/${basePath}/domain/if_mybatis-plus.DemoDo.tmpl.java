e929c623213a1d5d33eede486bb3b5bd448d52a5
taccisum <liaojinfeng@deepexi.com>
2019-07-03 12:34:13 +0800

    feat(scaffold): integrate lombok

--- generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/if_mybatis-plus.DemoDo.tmpl.java
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/if_mybatis-plus.DemoDo.tmpl.java
@@ -1,25 +1,11 @@
 package ${basePackage}.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 @TableName("demo")
+@Data
 public class DemoDo {
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/if_mybatis.DemoDo.tmpl.java
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/if_mybatis.DemoDo.tmpl.java
@@ -1,22 +1,9 @@
 package ${basePackage}.domain;
 
+import lombok.Data;
+
+@Data
 public class DemoDo {
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
