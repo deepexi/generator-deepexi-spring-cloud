9e248366ffdcd6401da7520f9eb14258cd0617da
taccisum <liaojinfeng@deepexi.com>
2019-07-09 17:47:55 +0800

    feat(scaffold): add TableId annotation to DemoDo field id

--- generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/if_mybatis-plus.DemoDo.tmpl.java
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/if_mybatis-plus.DemoDo.tmpl.java
@@ -1,11 +1,12 @@
 package ${basePackage}.domain;
 
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 @TableName("demo")
 @Data
 public class DemoDo {
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
 }
