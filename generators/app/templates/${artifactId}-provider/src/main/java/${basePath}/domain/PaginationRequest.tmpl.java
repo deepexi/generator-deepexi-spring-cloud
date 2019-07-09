8a96d663329875a7256fc2336cd9b720da05423a
taccisum <liaojinfeng@deepexi.com>
2019-07-09 17:16:24 +0800

    feat(scaffold): provide base class: PaginationRequest

---
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/PaginationRequest.tmpl.java
@@ -0,0 +1,25 @@
+package ${basePackage}.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiParam;
+import lombok.Data;
+
+@Data
+public abstract class PaginationRequest {
+    @ApiParam(value = "index", example = "1")
+    private Integer index = 1;
+    @ApiParam(value = "size", example = "20")
+    private Integer size = 20;
+
+    @JsonIgnore
+    @ApiParam(hidden = true)
+    public Integer getOffset() {
+        return getSize() * (getIndex() - 1);
+    }
+
+    @JsonIgnore
+    @ApiParam(hidden = true)
+    public Integer getLimit() {
+        return getSize();
+    }
+}
