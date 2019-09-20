b7e3e1838174cce0380b16d93337e16e25fdb56f
taccisum <liaojinfeng@deepexi.com>
2019-09-20 18:51:31 +0800

    fix: can not render path template on direct_th

--- generators/app/handler/direct_th.js
+++ generators/app/handler/direct_th.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const _ = require('lodash')
 const AbstractTemplateHandler = require('yo-power-generator').AbstractTemplateHandler;
 const fileUtils = require('yo-power-generator').FileUtils;
 
 class DirectTemplateHandler extends AbstractTemplateHandler {
   _handle0 () {
     this.generator.fs.write(
-      this.generator.destinationPath(fileUtils.tmplToFileName(this.tmpl)),
+      this.generator.destinationPath(_.template(fileUtils.tmplToFileName(this.tmpl))(this.props)),
       this.generator.fs.read(this.generator.templatePath(this.tmpl))
     )
   }
