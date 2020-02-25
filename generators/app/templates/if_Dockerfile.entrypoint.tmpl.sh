a89a73787b385dd1a78ac625640006b716513753
xieyuanqing <xieyuanqing@deepexi.com>
2020-02-25 17:10:37 +0800

    chore:  test

--- generators/app/templates/if_Dockerfile.entrypoint.tmpl.sh
+++ generators/app/templates/if_Dockerfile.entrypoint.tmpl.sh
@@ -6,7 +6,7 @@ if (apm === 'skywalking') {
     -javaagent:agent/skywalking/skywalking-agent.jar \\
     -Dskywalking.agent.service_name=\${SW_SERVICE_NAME:-${artifactId}} \\
     -Dskywalking.collector.backend_service=\${SW_SERVICE_ADDR} \\
-    -jar app.jar $@`)
+    -jar ------ app.jar $@`)
 } else {
     print(`java -jar app.jar $@`)
 }
