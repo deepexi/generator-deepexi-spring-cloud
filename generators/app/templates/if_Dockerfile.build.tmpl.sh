b5ed1c18eb72ee26438610ce9cbe371c25bc2fc4
xieyuanqing <xieyuanqing@deepexi.com>
2020-02-28 17:50:08 +0800

    refactor: adjust build.sh

--- generators/app/templates/if_Dockerfile.build.tmpl.sh
+++ generators/app/templates/if_Dockerfile.build.tmpl.sh
@@ -9,24 +9,15 @@ h2 '准备构建项目'
 
 if which mvn ; then
     info '使用本地maven构建项目'
-    mvn clean package -DskipTests \
+    mvn clean package -DskipTests
 else
     info '使用maven镜像['$img_mvn']构建项目'
     docker run --rm \
         -v $m2_cache:/root/.m2 \
         -v $PROJECT_HOME:/usr/src/mymaven \
         -w /usr/src/mymaven \
-        $img_mvn mvn clean package -DskipTests  \
+        $img_mvn mvn clean package -DskipTests
 fi
-<%
-    if ( docker !== 'Dockerfile') {
-        print(`
-          if [ ! -z $version ];then
-            -Ddocker.tag=$VERSION \\
-          fi
-        `)
-    }
-%>
 if [ $? -eq 0 ];then
     success '项目构建成功'
 else
@@ -34,20 +25,10 @@ else
     exit 1
 fi
 
-<%
-
-if (docker === 'Dockerfile') {
-
-  print(`
 h2 '准备构建Docker镜像'
 
 if [ ! -z $IMAGE_NAME ];then
     docker build --rm -t $IMAGE_NAME .
 else
-    docker build --rm -t $APP_NAME:v$VERSION .
+    docker build --rm -t $APP_NAME:$VERSION .
 fi
-  `)
-
-}
-
-%>
