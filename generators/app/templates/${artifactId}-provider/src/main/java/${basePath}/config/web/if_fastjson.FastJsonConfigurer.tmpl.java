fff20a509008c5389647269794973046a230b64b
xiangtch <imsmallmouse@gmail.com>
2020-03-05 22:45:01 +0800

    fix: 修复 FastJson 与 Swagger2 冲突问题

--- generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/config/web/if_fastjson.FastJsonConfigurer.tmpl.java
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/config/web/if_fastjson.FastJsonConfigurer.tmpl.java
@@ -4,8 +4,12 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class FastJsonConfigurer implements WebMvcConfigurer {
@@ -13,6 +17,9 @@ public class FastJsonConfigurer implements WebMvcConfigurer {
     public HttpMessageConverter<?> httpMessageConverter() {
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
         fastJsonHttpMessageConverter.setFastJsonConfig(new FastJsonConfig());
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
         return fastJsonHttpMessageConverter;
     }
 }
