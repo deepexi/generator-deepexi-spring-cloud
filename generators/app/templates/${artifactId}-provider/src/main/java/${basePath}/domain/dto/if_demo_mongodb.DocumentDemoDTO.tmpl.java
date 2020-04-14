5214d048543f428712135868765139c8d4469eec
taccisum <liaojinfeng@deepexi.com>
2020-04-14 12:32:47 +0800

    fix: missing import of @NotNull package

--- generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/dto/if_demo_mongodb.DocumentDemoDTO.tmpl.java
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/domain/dto/if_demo_mongodb.DocumentDemoDTO.tmpl.java
@@ -3,10 +3,11 @@ package ${basePackage}.domain.dto;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Accessors(chain = true)
 public class DocumentDemoDTO {
-
     @NotNull
     private String username;
 }
