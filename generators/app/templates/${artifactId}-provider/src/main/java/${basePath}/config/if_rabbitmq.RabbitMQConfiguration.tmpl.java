222e9ba287959bb0e5b6c88a83565f0c9c03cfe1
taccisum <liaojinfeng@deepexi.com>
2020-04-14 15:04:46 +0800

    fix: should only cover bean MessageConverter

--- generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/config/if_rabbitmq.RabbitMQConfiguration.tmpl.java
+++ generators/app/templates/${artifactId}-provider/src/main/java/${basePath}/config/if_rabbitmq.RabbitMQConfiguration.tmpl.java
@@ -1,28 +1,21 @@
 package ${basePackage}.config;
 
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConversionException;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
-        return rabbitTemplate;
-    }
-
-    @Bean("rabbitListenerContainerFactory")
-    SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        return factory;
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter() {
+            @Override
+            public Object fromMessage(Message message, Object conversionHint) throws MessageConversionException {
+                return message.getBody();
+            }
+        };
     }
 }
