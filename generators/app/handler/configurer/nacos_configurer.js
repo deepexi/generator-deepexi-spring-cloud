4aeb5c302805ca6df697d2cb73aba908613c4ac9
xiangtch <imsmallmouse@gmail.com>
2020-07-01 17:21:12 +0800

    chore: 修改 Nacos 归属注册中心和配置服务

--- generators/app/handler/configurer/nacos_configurer.js
+++ generators/app/handler/configurer/nacos_configurer.js
@@ -3,14 +3,26 @@ const _ = require('lodash');
 module.exports = {
   key: 'nacos',
   fn: {
-    configureProviderPomDependencies (optionalDependencies) {
-      optionalDependencies.push({
-        dependency: [
-          { groupId: 'com.alibaba.cloud' },
-          { artifactId: 'spring-cloud-starter-alibaba-nacos-config' },
-          { version: '2.0.2.RELEASE' }
-        ]
-      })
+    configureProviderPomDependencies (optionalDependencies, props) {
+      if (props.openNacosDiscovery) {
+        optionalDependencies.push({
+          dependency: [
+            { groupId: 'com.alibaba.cloud' },
+            { artifactId: 'spring-cloud-starter-alibaba-nacos-discovery' },
+            { version: '2.0.2.RELEASE' }
+          ]
+        })
+      }
+
+      if (props.openNacosConfigservice) {
+        optionalDependencies.push({
+          dependency: [
+            { groupId: 'com.alibaba.cloud' },
+            { artifactId: 'spring-cloud-starter-alibaba-nacos-config' },
+            { version: '2.0.2.RELEASE' }
+          ]
+        })
+      }
     },
     configureBootstrapYaml (yaml, props) {
       _.merge(yaml, {
